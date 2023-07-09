3a2485326562e460185b1d272fbdf61e698366e5
System Administrator <root@MacBook-Air-Admin.local>
2023-07-09 11:11:36 +0300

    dev: techs basic layout done

--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import './Techs.css'
 
 export default function Techs() {
     return (
-        <div>Techs</div>
+        <section className='techs'>
+            <h2 className='techs__title'>Технологии</h2>
+            <h2 className='techs__description-title'>7 технологий</h2>
+            <p className='techs__description-text'>На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
+            <ul className='techs__list'>
+                <li className='list__item'>HTML</li>
+                <li className='list__item'>CSS</li>
+                <li className='list__item'>JS</li>
+                <li className='list__item'>React</li>
+                <li className='list__item'>Git</li>
+                <li className='list__item'>Express.js</li>
+                <li className='list__item'>MongoDB</li>
+            </ul>
+
+        </section>
     )
 }
