3652d54a6016465f20ca44c9164bb50139a7217b
System Administrator <root@MacBook-Air-Admin.local>
2023-07-09 09:49:47 +0300

    dev: promo layout fixed

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -1,7 +1,32 @@
 import React from 'react'
+import './AboutProject.css'
 
 export default function AboutProject() {
     return (
-        <div>AboutProject</div>
+        <section className='about-project'>
+            <h2 className='about-project__title'>О проекте</h2>
+
+            <div className='about-project__articles'>
+                <article className='about-project__article'>
+                    <h3 className='article__header'>Дипломный проект включал 5 этапов</h3>
+                    <p className='article__text'>Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+                </article>
+                <article className='about-project__article'>
+                    <h3 className='article__header'>На выполнение диплома ушло 5 недель</h3>
+                    <p className='article__text'>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+                </article>
+            </div>
+
+            <div className='about-project__duration'>
+                <div className='duration__one-week'>
+                    <p className='duration__time time__week'>1 неделя</p>
+                    <p className='duration__stage-name'>Back-end</p>
+                </div>
+                <div className='duration__four-week'>
+                    <p className='duration__time time__four-weeks'>4 недели</p>
+                    <p className='duration__stage-name'>Front-end</p>
+                </div>
+            </div>
+        </section>
     )
 }
--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -6,7 +6,7 @@ export default function Promo() {
     return (
         <section className='promo'>
             <h1 className='promo__title'>Учебный проект студента факультета Веб-разработки.</h1>
-            <img className='promo__image' src={promoImage} alt='промо картинка' />
+            <div className='promo__image'></div>
         </section>
     )
 }
