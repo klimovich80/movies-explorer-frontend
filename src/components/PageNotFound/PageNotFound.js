d7f1e0f2ad0475610bdb21cebbfd9a4ef7779597
System Administrator <root@MacBook-Air-Admin.local>
2023-07-17 07:49:55 +0300

    dev: 404 pp check done

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,17 +1,12 @@
 import './PageNotFound.css'
-import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 export default function PageNotFound() {
-    const navigate = useNavigate();
-
-    function goBack() {
-        navigate(-1);
-    }
 
     return (
         <section className='page-not-found'>
             <h2 className='page-not-found__title'>404</h2>
             <p className='page-not-found__text'>Страница не найдена</p>
-            <button className='page-not-found__button' onClick={goBack}>Назад</button>
+            <Link className='page-not-found__link link' to={-1}>Назад</Link>
         </section>
     )
 }
