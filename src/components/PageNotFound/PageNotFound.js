31d77754325d8495b9e5e5dc0854605435bc9173
System Administrator <root@MacBook-Air-Admin.local>
2023-07-17 07:28:41 +0300

    dev: 404 layout  done

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,6 +1,17 @@
-
+import './PageNotFound.css'
+import { useNavigate } from 'react-router-dom'
 export default function PageNotFound() {
+    const navigate = useNavigate();
+
+    function goBack() {
+        navigate(-1);
+    }
+
     return (
-        <div>PageNotFound</div>
+        <section className='page-not-found'>
+            <h2 className='page-not-found__title'>404</h2>
+            <p className='page-not-found__text'>Страница не найдена</p>
+            <button className='page-not-found__button' onClick={goBack}>Назад</button>
+        </section>
     )
 }
