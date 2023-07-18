9d08d83c8bb79dd4d41e68e9912b59a4ebea8f36
System Administrator <root@MacBook-Air-Admin.local>
2023-07-18 16:56:31 +0300

    fix: search form validation done

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,17 +1,59 @@
 import './SearchForm.css'
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox'
+import MyInput from '../UI/MyInput/MyInput'
+import { useEffect } from 'react'
+import useForm from '../hooks/useForm'
 
 export default function SearchForm({ isShort, setShort }) {
+
+    const buttonText = 'Поиск';
+
+    const { values, errors, handleChange } = useForm({
+        movie: ''
+    });
+
+    useEffect(() => {
+        values.movie = '';
+        errors.movie = '';
+    }, []);
+
+    const disableButton = errors.movie !== '';
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log('submitting search form');
+        console.log(values);
+    }
+
     return (
         <section className='search'>
             <form className='search__form'>
                 <div className='search__container'>
-                    <input
-                        className='search__input'
+                    <MyInput
+                        id='search__movie'
+                        name="movie"
+                        error={errors.movie}
                         type='search'
+                        required
+                        minLength="1"
+                        maxLength="50"
                         placeholder='Фильм'
+                        value={values.name}
+                        onChange={handleChange}
                     />
-                    <button className='search__button button' type='submit' aria-label='Поиск'>Поиск</button>
+                    <button
+                        className={
+                            disableButton
+                                ? 'search__button search__button_disabled button'
+                                : 'search__button button'
+                        }
+                        type='submit'
+                        aria-label={buttonText}
+                        disabled={disableButton}
+                        onClick={handleSubmit}
+                    >
+                        {buttonText}
+                    </button>
                 </div>
                 <FilterCheckbox isShort={isShort} setShort={setShort} />
             </form>
